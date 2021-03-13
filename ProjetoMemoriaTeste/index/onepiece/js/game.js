f4eb1eeff268f6701e25561ba90cc51d67b19ea2
Breno-Ramos-dos-Santos <79485719+Breno-Ramos-dos-Santos@users.noreply.github.com>
2021-03-13 19:39:09 -0300

    Update game.js

--- ProjetoMemoriaTeste/index/onepiece/js/game.js
+++ ProjetoMemoriaTeste/index/onepiece/js/game.js
@@ -152,4 +152,5 @@ var contador = document.querySelector('.badge');
 document.querySelector('#tabuleiro').addEventListener('click', function(){
   var numero = parseInt(contador.textContent) + 1 ;
   contador.textContent = numero  ;
+    });
 
